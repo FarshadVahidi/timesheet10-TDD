702f83cc48f2766accad8a7a42b1fc5485f32eca
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-20 14:10:35 +0100

    RED: super admin can add new hour

--- tests/Feature/superAdministratorTest.php
+++ tests/Feature/superAdministratorTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Hour;
 use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
@@ -18,9 +19,9 @@ class superAdministratorTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $user = User::factory()->create();
-        $role = Role::factory()->create(['name' => 'superadministrator']);
-        $permit = Permission::factory()->create(['name'=>'superadministrator']);
+        $user = $this->getModel();
+        $role = $this->getRoleSuper();
+        $permit = $this->getPermitCreateHour();
         $role->attachPermission($permit);
 
         $this->actingAs($user)->get('/dashboard')->assertRedirect('login');
@@ -35,9 +36,9 @@ class superAdministratorTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $user = User::factory()->create();
-        $role = Role::factory()->create(['name' => 'superadministrator']);
-        $permit = Permission::factory()->create(['name'=>'superadministrator']);
+        $user = $this->getModel();
+        $role = $this->getRoleSuper();
+        $permit = $this->getPermitCreateHour();
 
         $this->actingAs($user)->get('/addNewHour')->assertRedirect(route('login'));
 
@@ -48,4 +49,52 @@ class superAdministratorTest extends TestCase
         $response->assertViewIs('super.addHour');
         $response->assertSee('add new hour');
     }
+
+    /** @test */
+    public function super_administrator_can_add_hour_POST()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = $this->getModel();
+        $role = $this->getRoleSuper();
+        $permit = $this->getPermitCreateHour();
+
+        $response = $this->actingAs($user)->post('createNewHour', ['user_id' => $user->id, 'date' => '1983/02/01', 'hour' => 800]);
+        $response->assertSessionHas('RED');
+
+        $role->attachPermission($permit);
+        $user->attachRole($role);
+
+        $this->actingAs($user)->post('/createNewHour', ['user_id' => $user->id, 'date' => '1983/02/01', 'hour' => 800]);
+
+        $hour = Hour::first();
+
+        $this->assertCount(1, Hour::all());
+        $this->assertEquals($user->id, $hour->user_id );
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    private function getModel()
+    {
+        return User::factory()->create();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    private function getRoleSuper()
+    {
+        return Role::factory()->create(['name' => 'superadministrator']);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    private function getPermitCreateHour()
+    {
+        return Permission::factory()->create(['name' => 'hour-create']);
+    }
+
 }
