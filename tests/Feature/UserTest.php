a9a5b5b4acd442cca6337f3f06db5edc41c168d0
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-23 13:26:17 +0100

    RED:

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /** @test */
+    public function administrator_must_first_login()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = $this->getModel();
+        $role = $this->getRoleUser();
+
+        $this->actingAs($user)->get('/dashboard')->assertRedirect('login');
+
+        $user->attachRole($role);
+        $response = $this->actingAs($user)->get('/dashboard')->assertViewIs('user.dashboard');
+        $response->assertSee('user dashboard');
+    }
+
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    private function getModel()
+    {
+        return User::factory()->create();
+    }
+
+    private function getRoleUser()
+    {
+        return Role::factory()->create(['name' => 'user']);
+    }
+}
