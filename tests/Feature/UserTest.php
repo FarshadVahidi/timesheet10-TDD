ccd821d2a1478912c11be53069a40de09011ad44
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-23 13:36:17 +0100

    RED: addHour for user

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Hour;
+use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -29,6 +31,49 @@ class UserTest extends TestCase
     }
 
 
+    /** @test */
+    public function user_can_add_hour()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = $this->getModel();
+        $role = $this->getRoleUser();
+        $permit = $this->getPermitCreateHour();
+
+        $this->actingAs($user)->get('/addNewHour')->assertRedirect(route('login'));
+
+        $role->attachPermission($permit);
+        $user->attachRole($role);
+
+        $response = $this->actingAs($user)->get('/addNewHour');
+        $response->assertViewIs('user.addHour');
+        $response->assertSee('add new hour');
+    }
+
+
+    /** @test */
+    public function user_can_add_hour_POST()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = $this->getModel();
+        $role = $this->getRoleUser();
+        $permit = $this->getPermitCreateHour();
+
+        $response = $this->actingAs($user)->post('createNewHour', ['user_id' => $user->id, 'date' => '1983/02/01', 'hour' => 800, 'nonWork' => 0]);
+        $response->assertSessionHas('RED');
+
+        $role->attachPermission($permit);
+        $user->attachRole($role);
+
+        $this->actingAs($user)->post('/createNewHour', ['user_id' => $user->id, 'date' => '1983/02/01', 'hour' => 800, 'nonWork' => 0]);
+
+        $hour = Hour::first();
+
+        $this->assertCount(1, Hour::all());
+        $this->assertEquals($user->id, $hour->user_id );
+    }
+
 
     /**
      * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
@@ -41,5 +86,13 @@ class UserTest extends TestCase
     private function getRoleUser()
     {
         return Role::factory()->create(['name' => 'user']);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    private function getPermitCreateHour()
+    {
+        return Permission::factory()->create(['name' => 'hour-create']);
     }
 }
