6e149533f3afc7177381a8c8970d837b4e5cef7a
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-23 11:59:50 +0100

    RED

--- tests/Feature/AdministratorTest.php
+++ tests/Feature/AdministratorTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,7 +15,7 @@ class AdministratorTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function super_administrator_must_first_login()
+    public function administrator_must_first_login()
     {
         $this->withoutExceptionHandling();
 
@@ -29,7 +30,25 @@ class AdministratorTest extends TestCase
     }
 
 
-    
+    /** @test */
+    public function administrator_can_add_hour()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = $this->getModel();
+        $role = $this->getRoleAdmin();
+        $permit = $this->getPermitCreateHour();
+
+        $this->actingAs($user)->get('/addNewHour')->assertRedirect(route('login'));
+
+        $role->attachPermission($permit);
+        $user->attachRole($role);
+
+        $response = $this->actingAs($user)->get('/addNewHour');
+        $response->assertViewIs('admin.addHour');
+        $response->assertSee('add new hour');
+    }
+
 
     /**
      * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
@@ -37,6 +56,14 @@ class AdministratorTest extends TestCase
     private function getModel()
     {
         return User::factory()->create();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|mixed
+     */
+    private function getPermitCreateHour()
+    {
+        return Permission::factory()->create(['name' => 'hour-create']);
     }
 
     private function getRoleAdmin()
